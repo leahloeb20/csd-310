6db94b26fdc6e54e52ebcc8c3f1888451aa31039
Leah Herschmann <95591519+leahloeb20@users.noreply.github.com>
2022-02-09 15:44:32 +0200

    commit on 'main'

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,6 @@
+import pymongo
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.kixz7.mongodb.net/pytech?retryWrites=true&w=majority";
+client = MongoClient(url)
+db = client.pytech
+print(db.list_collection_names)
