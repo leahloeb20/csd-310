96ce738a0ce6d4a3f3f3f5ed50d73e7ba6d86a72
Leah Loeb <95591519+leahloeb20@users.noreply.github.com>
2022-02-21 21:59:57 +0200

    table

--- module_7/db_init.sql
+++ module_7/db_init.sql
@@ -1,2 +1,23 @@
-player
-team
+--drop table if exists
+DROP TABLE IF EXISTS player;
+DROP TABLE IF EXISTS team;
+
+--create the team table
+CREATE TABLE team (
+    team_id     INT            NOT NULL     AUTO_INCREMENT,
+    team_name   VARCHAR(75)    NOT NULL,
+    mascot      VARCHAR(75)    NOT NULL,
+    PRIMARY KEY(team_id)
+);
+
+--create the player table and set the foreign key
+CREATE TABLE player (
+    player_id   INT            NOT NULL      AUTO_INCREMENT,
+    first_name  VARCHAR(75)    NOT NULL,
+    last_name   VARCHAR(75)    NOT NULL,
+    team_id     INT            NOT NULL,
+    PRIMARY KEY(player_id)
+    CONSTRAINT fk_team
+    FOREIGN KEY(team_id)
+        REFERENCES team(team_id)
+);
