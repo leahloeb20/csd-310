29eaac29e9d485f94c1a5eb18f247cd611247eb7
Leah Loeb <95591519+leahloeb20@users.noreply.github.com>
2022-02-21 21:31:14 +0200

    fix

--- module_7/db_init.sql
+++ module_7/db_init.sql
@@ -1,16 +1,2 @@
-+<diagram program="umletino" version="14.4.0-SNAPSHOT"><zoom_level>10</zoom_level><element><id>UMLClass</id><coordinates><x>480</x><y>180</y><w>200</w><h>100</h></coordinates><panel_attributes>Player Fields
-
---
-player_id
---
-first_name
---
-last_name</panel_attributes><additional_attributes></additional_attributes></element><element><id>UMLClass</id><coordinates><x>90</x><y>180</y><w>190</w><h>100</h></coordinates><panel_attributes>Team Fields
-
---
-team_id
---
-team_name
---
-mascot</panel_attributes><additional_attributes></additional_attributes></element><element><id>Relation</id><coordinates><x>270</x><y>210</y><w>280</w><h>40</h></coordinates><panel_attributes></panel_attributes><additional_attributes>260;20;10;20</additional_attributes></element><element><id>Text</id><coordinates><x>280</x><y>230</y><w>100</w><h>70</h></coordinates><panel_attributes>1.1
-</panel_attributes><additional_attributes></additional_attributes></element><element><id>Text</id><coordinates><x>450</x><y>230</y><w>100</w><h>70</h></coordinates><panel_attributes>1.*</panel_attributes><additional_attributes></additional_attributes></element></diagram>
+player
+team
